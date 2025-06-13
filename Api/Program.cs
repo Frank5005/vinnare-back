95c36824aa018de0b76b5ef8ea8dbb5a19b23212
Frank5005 <frankill.5005@gmail.com>
2025-06-13 17:42:08 -0500

    Updating host

--- Api/Program.cs
+++ Api/Program.cs
@@ -28,7 +28,7 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins(
             "http://localhost:8080",
-            //"https://main.d3hcv6qzhmyahb.amplifyapp.com",
+            //"https://main.d3hcv6qzhmyahb.amplifyapp.comaaa",
             //"https://5586-3-147-45-32.ngrok-free.app"
             "https://49b9-3-144-90-59.ngrok-free.app"
         )
